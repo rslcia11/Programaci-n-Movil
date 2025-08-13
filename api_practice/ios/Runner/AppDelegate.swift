bda67b209535edfcb38ba9297b7b9522e3c6d76c
Wilson Martinez <133081999+rslcia11@users.noreply.github.com>
2025-08-13 00:11:35 -0500

    Geolocalizacion

--- api_practice/ios/Runner/AppDelegate.swift
+++ api_practice/ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     // Proveer la API Key de Google Maps aquí
-    GMSServices.provideAPIKey("AIzaSyBWdrDGZu3_Y4HhAe4QiUTNPV-GT64c6Ew")
+    GMSServices.provideAPIKey("INTRODUCIR LA API KEY DE GOOGLE MAPS AQUI")
     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
